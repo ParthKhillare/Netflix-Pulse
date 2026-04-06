6ccb421b91585cfb1a289ef5fdc50f983d35940f
Kaze <parthkhillareiiitb@gmail.com>
2026-04-06 19:49:40 +0530

    Delete src/vite-env.d.ts

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_GEMINI_API_KEY: AIzaSyC08YzPaly2ZMEQSZSEEjwR2ngI82r3e7c
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
